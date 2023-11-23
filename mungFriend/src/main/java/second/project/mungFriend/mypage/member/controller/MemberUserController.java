16f20cf2a9a97ea7efbd727dc7d604171e1813b1
yujinjjang2 <o3odw98@gmail.com>
2023-11-23 17:38:48 +0900

    update aca

--- mungFriend/src/main/java/second/project/mungFriend/mypage/member/controller/MemberUserController.java
+++ mungFriend/src/main/java/second/project/mungFriend/mypage/member/controller/MemberUserController.java
@@ -1,12 +1,24 @@
 package second.project.mungFriend.mypage.member.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
+
+import second.project.mungFriend.mypage.member.model.dto.MemberUser;
+import second.project.mungFriend.mypage.member.model.service.MemberUserService;
 
 @Controller
 @RequestMapping("/mypage/member")
+@SessionAttributes({"loginMember"})
 public class MemberUserController {
+	
+	@Autowired
+	private MemberUserService service;
 	
 	// 정보수정 화면 전환
 	@GetMapping("/updateInfoUser")
@@ -23,5 +35,17 @@ public class MemberUserController {
 	}
 	
 	// 회원탈퇴
+	@PostMapping("/secessionUser")
+	public String secessionUser(String memberPw
+			,@SessionAttribute("loginMember") MemberUser loginMember
+			,SessionStatus status) {
+		
+		int memberNo = loginMember.getMemberNo();
+		
+		int result = service.secessionUser(memberPw, memberNo);
+		
+		return "";
+		
+	}
 
 }
--- mungFriend/src/main/java/second/project/mungFriend/mypage/member/model/service/MemberUserServiceImpl.java
+++ mungFriend/src/main/java/second/project/mungFriend/mypage/member/model/service/MemberUserServiceImpl.java
@@ -1,5 +1,22 @@
 package second.project.mungFriend.mypage.member.model.service;
 
-public class MemberUserServiceImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import second.project.mungFriend.mypage.member.model.dao.MemberUserDAO;
+
+@Service
+public class MemberUserServiceImpl implements MemberUserService{
+	
+	@Autowired
+	private MemberUserDAO dao;
+
+	// 회원탈퇴
+	@Override
+	public int secessionUser(String memberPw, int memberNo) {
+		return 0;
+	}
+	
+	
 
 }
