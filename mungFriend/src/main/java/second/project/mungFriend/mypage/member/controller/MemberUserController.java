9c2d789184c8164dae908cbe83d09d9e1e424973
aspyn1 <aspynisgood@gmail.com>
2023-11-24 17:29:09 +0900

    jieun 1124 1729

--- mungFriend/src/main/java/second/project/mungFriend/mypage/member/controller/MemberUserController.java
+++ mungFriend/src/main/java/second/project/mungFriend/mypage/member/controller/MemberUserController.java
@@ -8,6 +8,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/mypage/member")
 public class MemberUserController {
 	
+	// 찜한목록 화면 전환
+	@GetMapping("/myPageLikeList")
+	public String myPageLikeList() {
+		return "mypage/member/myPageLikeList";
+	}
+	
 	// 정보수정 화면 전환
 	@GetMapping("/updateInfoUser")
 	public String updateInfoUserPage() {
