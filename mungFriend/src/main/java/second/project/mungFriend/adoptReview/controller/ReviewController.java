89131539421d815dc5d910a581c1457fa7212784
doyeon <zzangkidy@naver.com>
2023-11-24 17:33:34 +0900

    1124

--- mungFriend/src/main/java/second/project/mungFriend/adoptReview/controller/ReviewController.java
+++ mungFriend/src/main/java/second/project/mungFriend/adoptReview/controller/ReviewController.java
@@ -1,5 +1,6 @@
 package second.project.mungFriend.adoptReview.controller;
 
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,11 +8,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import second.project.mungFriend.adoptReview.model.dto.Review;
 import second.project.mungFriend.adoptReview.model.service.ReviewService;
+import second.project.mungFriend.member.model.dto.Member;
 
 @SessionAttributes({"loginMember"})
 @RequestMapping("/adoptReview")
@@ -22,19 +29,18 @@ public class ReviewController {
 	private ReviewService service;
 	
 	
-	@GetMapping("/reviewList")
+	
+	@GetMapping("/reviewList/{cp}")
 	public String selectReviewList(
-			@RequestParam(value="cp", required = false, defaultValue = "1") int cp,
-			@RequestParam(value = "searchContent", required = false) String searchContent,
-			@RequestParam(value = "searchType", required = false) String searchType,
+			@PathVariable(value="cp", required = false) int cp,
+			@RequestParam Map<String, Object> paramMap,
 			Model model
 			) {
 			
 			System.out.println("현재페이지 : " + cp);
-			System.out.println("searchContent : " + searchContent);
-			System.out.println("searchType : " + searchType);
 			
-			if(searchContent == null) { // 검색어가 없을 때 
+			
+			if(paramMap.get("searchContent") == null) { // 검색어가 없을 때 
 				
 				
 				// 게시글 목록 조회 서비스 호출
@@ -49,10 +55,13 @@ public class ReviewController {
 			}else {  //검색어 있을 때
 				
 				
-				Map<String, Object> map = service.selectSearchReviewList(searchType,searchContent,cp);
+//				Map<String, Object> map = service.selectSearchReviewList(searchType,searchContent,cp);
+//				
+//				map.put("searchType", searchType);
+//				map.put("searchContent", searchContent);
 				
-				map.put("searchType", searchType);
-				map.put("searchContent", searchContent);
+				Map<String, Object> map = service.selectSearchReviewList(paramMap, cp);
+				
 				model.addAttribute("map", map);
 				
 			}
@@ -70,6 +79,35 @@ public class ReviewController {
 		
 		return"adoptReview/ReviewDetail";
 	}
+	
+	
+	
+	@GetMapping("/reviewInsert")
+	public String reviewInsert() {
+		
+		return "adoptReview/ReviewCreate";
+	}
+	
+	
+	@PostMapping("/reviewInsert")
+	public String reviewInsert( Review review
+			, @RequestParam(value="images", required = false) List<MultipartFile> images
+			, @SessionAttribute("loginMember") Member loginMember
+			, RedirectAttributes ra
+			)throws Exception {
+		
+		System.out.println("등록하려는 값 : " + review);
+		System.out.println("이미지 리스트 : " + images);
+		
+		review.setMemberNo(loginMember.getMemberNo());
+		review.setMemberNickname(loginMember.getMemberNickname());
+		
+		int insertResult = service.reviewInsert(review,images);
+		
+		
+		return"redirect:reviewList/1";
+	}
+	
 	
 	
 	
--- mungFriend/src/main/java/second/project/mungFriend/adoptReview/model/service/ReviewServiceImpl.java
+++ mungFriend/src/main/java/second/project/mungFriend/adoptReview/model/service/ReviewServiceImpl.java
@@ -1,25 +1,44 @@
 package second.project.mungFriend.adoptReview.model.service;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.apache.ibatis.session.RowBounds;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
+import second.project.mungFriend.admissionApply.model.exception.FileUploadException;
 import second.project.mungFriend.adopt.model.dto.Pagination;
 import second.project.mungFriend.adoptReview.model.dao.ReviewMapper;
 import second.project.mungFriend.adoptReview.model.dto.Review;
+import second.project.mungFriend.adoptReview.model.dto.ReviewImage;
+import second.project.mungFriend.common.utility.Util;
 
 @Service
+@PropertySource("classpath:/config.properties")
 public class ReviewServiceImpl implements ReviewService{
 
 	@Autowired
 	private ReviewMapper mapper;
 
+	@Value("${my.adopt.webpath}")
+	private String webPath;
+	
+	@Value("${my.adopt.location}")
+	private String filePath;
 	
 	
+	/**
+	 * 검색어 없을 시 게시글 조회
+	 */
 	@Override
 	public Map<String, Object> selectReviewList(int cp) {
 		
@@ -58,10 +77,114 @@ public class ReviewServiceImpl implements ReviewService{
 	}
 
 	
+
+	
+	/**
+	 * 검색어 있을 시 게시글 조회
+	 */
 	@Override
-	public Map<String, Object> selectSearchReviewList(String searchType, String searchContent, int cp) {
-		// TODO Auto-generated method stub
-		return null;
+	public Map<String, Object> selectSearchReviewList(Map<String, Object> paramMap, int cp) {
+		
+		int listCount = mapper.getSearchListCount(paramMap); 
+		
+		Pagination pagination = new Pagination(listCount, cp);
+		
+		// 1) offset 계산
+		int offset 
+			= (pagination.getCurrentPage() - 1) * pagination.getLimit();
+		
+		// 2) RowBounds 객체 생성
+		RowBounds rowBounds = new RowBounds(offset, pagination.getLimit());
+		
+		List<Review> reviewList = mapper.selectSearchReviewList(paramMap,rowBounds);
+		
+		// 4. pagination, boardList를 Map에 담아서 반환
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("pagination", pagination);
+		map.put("reviewList", reviewList);
+		
+		
+		return map;
+	}
+
+
+	
+	/**
+	 * 게시글 삽입
+	 * @throws Exception 
+	 * @throws IllegalStateException 
+	 */
+	@Transactional(rollbackFor = Exception.class)
+	@Override
+	public int reviewInsert(Review review, List<MultipartFile> images) throws IllegalStateException, Exception {
+		
+		int result = mapper.reviewInsert(review);
+		
+		if(result == 0 ) {return 0;}
+		
+		int reviewNo = review.getReviewNo();
+		
+		//게시글 삽입 성공 시 업로드된 이미지가 있다면 Review_Img 테이블에 삽입
+				if(reviewNo > 0) { 
+					
+					// 실제 업로드될 파일의 정보를 기록할 list
+					List<ReviewImage> uploadList = new ArrayList<ReviewImage>();
+					
+					
+					for(int i=0; i<images.size(); i++ ) {	
+						
+						if(images.get(i).getSize() > 0) {
+							
+							ReviewImage img = new ReviewImage();
+							
+							// img 에 파일 정보 담아서 uploadList 에 추가
+							img.setImagePath(webPath); //웹 접근경로
+							img.setImageNo(reviewNo);  //게시글 번호
+							img.setImageOrder(i); // 이미지 순서
+							
+							String fileName = images.get(i).getOriginalFilename();
+							
+							img.setImageOriginal(fileName);  //원본명
+							img.setImageReName(Util.fileRename(fileName)); // 변경명
+							
+							uploadList.add(img);
+							System.out.println("img 정보 : " + img );
+							
+						}
+							
+					}
+					
+					
+					if( !uploadList.isEmpty() ) {
+						
+						 result = mapper.insertImageList(uploadList);
+						// result : 삽입된 행의 개수
+						
+						if(result == uploadList.size()) {
+							
+							// 	uploadList 크기만큼 전부 insert가 수행됐다면 => insert 성공
+							for(int i=0; i<uploadList.size(); i++) {
+								
+								int index = uploadList.get(i).getImageOrder();
+								
+								String rename = uploadList.get(i).getImageReName();
+								
+								images.get(index).transferTo(new File(filePath + rename));
+							}
+							
+						}else { 
+							
+							throw new FileUploadException();
+						}
+						
+						
+					}
+					
+					
+					
+				}
+				
+		return 0;
 	}
 
 }
