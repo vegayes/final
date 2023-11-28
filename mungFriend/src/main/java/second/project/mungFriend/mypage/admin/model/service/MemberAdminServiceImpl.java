7e7f0d100dbb860bb5b2e1cbcef9323c72e46ee9
yujinjjang2 <o3odw98@gmail.com>
2023-11-28 17:18:28 +0900

    update aca yujin

--- mungFriend/src/main/java/second/project/mungFriend/mypage/admin/model/service/MemberAdminServiceImpl.java
+++ mungFriend/src/main/java/second/project/mungFriend/mypage/admin/model/service/MemberAdminServiceImpl.java
@@ -68,4 +68,35 @@ public class MemberAdminServiceImpl implements MemberAdminService{
 		return dao.memberDel(memberNo);
 	}
 
+	// 회원리스트 조회하기 (검색)
+	@Override
+	public Map<String, Object> selectMemberList(int cp, String memberSearch) {
+		
+		// 1. 탈퇴하지 않았고, 검색 조건이 일치하는 회원 리스트 수 조회
+		int listCount = dao.getSearchListCount(memberSearch); // 오버로딩
+		
+		// 2. 1번 조회 결과 + cp를 이용해서 Pagination 객체 생성
+		// -> 내부 필드가 모두 계산되어 초기화됨
+		Pagination pagination = new Pagination(listCount, cp);
+		
+		// 현재 페이지에 해당하는 부분에 대한 회원 리스트 조회
+		// 단, 검색 조건 일치하는 것만
+		
+		// 1) offset 계산
+		int offset
+			= (pagination.getCurrentPage() - 1) * pagination.getLimit();
+		
+		// 2) RowBounds 객체 생성
+		RowBounds rowBounds = new RowBounds(offset, pagination.getLimit());
+		
+		List<Member> memberList = dao.selectSearchMemberList(memberSearch, rowBounds);
+		
+		// 4. pagination, memberList를 Map에 담아서 반환
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("pagination", pagination);
+		map.put("memberList", memberList);
+		
+		return map;
+	}
+
 }
