010283327b124a2c975e0ea45323395ef1578fb7
aspyn1 <aspynisgood@gmail.com>
2023-11-29 17:29:13 +0900

    jieun 1129 1729

--- mungFriend/src/main/java/second/project/mungFriend/adoptReview/model/service/ReviewServiceImpl.java
+++ mungFriend/src/main/java/second/project/mungFriend/adoptReview/model/service/ReviewServiceImpl.java
@@ -1,7 +1,6 @@
 package second.project.mungFriend.adoptReview.model.service;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
--- mungFriend/src/main/java/second/project/mungFriend/mypage/member/controller/MemberUserController.java
+++ mungFriend/src/main/java/second/project/mungFriend/mypage/member/controller/MemberUserController.java
@@ -21,11 +21,11 @@ public class MemberUserController {
   @Autowired
 	private MemberUserService service;
   
-	// 찜한목록 화면 전환
-	@GetMapping("/myPageLikeList")
-	public String myPageLikeList() {
-		return "mypage/member/myPageLikeList";
-	}
+//	// 찜한목록 화면 전환
+//	@GetMapping("/myPageLikeList")
+//	public String myPageLikeList() {
+//		return "mypage/member/myPageLikeList";
+//	}
 
 
 	// 정보수정 화면 전환
