96315bb4318466311449183a30ce068affc09cbf
vegayes <vegayes@naver.com>
2023-12-14 17:35:14 +0900

    eunseo 12141

--- mungFriend/src/main/resources/static/js/centerInfo/map.js
+++ mungFriend/src/main/resources/static/js/centerInfo/map.js
@@ -141,7 +141,7 @@ function getListItem(index, places) {
 function addMarker(position, idx, title) {
 	
 	
-	var imageSrc = 'https://i.imgur.com/Jo326SS.png', // 마커 이미지 url, 스프라이트 이미지를 씁니다
+	var imageSrc = 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_number_blue.png', // 마커 이미지 url, 스프라이트 이미지를 씁니다
 		imageSize = new kakao.maps.Size(36, 37),  // 마커 이미지의 크기
 		imgOptions = {
 			spriteSize: new kakao.maps.Size(36, 691), // 스프라이트 이미지의 크기
